11211613a6747fee1f2177b4a99a82a8b34b641d
Imkaroboi <sarveshshirashyad@gmail.com>
2024-12-01 22:18:43 -0800

    working on looping through the array and displaying the item on the page

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-const myLibrary = [];
 
-function Book(title, author, pages, status) {
+const myLibrary = [];
+const container = document.querySelector('.container');
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.status = status;
+    this.read = read;
 };
 
 function addBookToLibrary() {
+    let pages = document.getElementById('Pages').value;
+    let name = document.getElementById('Name').value;
+    let author = document.getElementById('Author').value;
+    let read = document.getElementById('read').value;
+    let newBook = new Book(name, author, pages, read)
+    console.log(newBook);
+    myLibrary.push(newBook);
+    dialog.close()
 
+    for (let i = 0; i < myLibrary.length; i++) {
+        let box = document.createElement('div');
+        container.appendChild(box);
+        let text = document.createElement('p');
+        box.appendChild(text)
+        text.textContent = author;
+    }
 };
+
+
 const dialogDiv = document.querySelector('dialog');
 const dialog = document.getElementById('book-dialog');
+
+
+
 function show() {
 dialogDiv.style.visibility = "shown";
 dialog.showModal() 
@@ -19,3 +40,13 @@ dialog.showModal()
 function hide() {
     dialog.close()
 }
+
+// function bookItem() {
+    
+
+
+// const form = document.getElementById('forms');
+// form.addEventListener('submit', function (e){
+//     e.preventDefault();
+//     dialog.close()
+// })}
