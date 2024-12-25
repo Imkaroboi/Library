22997506633e8126158d0d6e8684fe9d0ab44f96
Imkaroboi <sarveshshirashyad@gmail.com>
2024-12-24 22:04:37 -0800

    built the base for the code to display on the page

--- main.js
+++ main.js
@@ -1,30 +1,35 @@
 
 const myLibrary = [];
 const container = document.querySelector('.container');
+const totalbooks = document.getElementById('totalbooks')
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 };
-
+function display() {
+    let allBooks = document.getElementById("totalbooks");
+    allBooks.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        let box = document.createElement('div');
+        totalbooks.appendChild(box);
+        let book = myLibrary[i];
+        box.innerHTML = `<p>${book.title}</p>`;
+        let paragraph = document.createElement('p');
+        paragraph.textContent = `${book.author}`;
+        box.appendChild(paragraph)
+        console.log(myLibrary[i])
+    }
+}
 function addBookToLibrary() {
     let pages = document.getElementById('Pages').value;
     let name = document.getElementById('Name').value;
     let author = document.getElementById('Author').value;
     let read = document.getElementById('read').value;
     let newBook = new Book(name, author, pages, read)
-    console.log(newBook);
     myLibrary.push(newBook);
     dialog.close()
-
-    // for (let i = 0; i < myLibrary.length; i++) {
-    //     let box = document.createElement('div');
-    //     container.appendChild(box);
-    //     let text = document.createElement('p');
-    //     box.appendChild(text)
-    //     text.textContent = author;
-    // }
     display()
 };
 
@@ -42,21 +47,6 @@ function hide() {
     dialog.close()
 }
 
-function display() {
-    let allBooks = document.getElementById("#totalbooks");
-    allBooks.innerHTML = ""
-    for (let i = 0; i < myLibrary.length; i++) {
-        // let box = document.createElement('div');
-        // container.appendChild(box);
-        // let text = document.createElement('p');
-        // box.appendChild(text)
-        // text.textContent = author;
-        let book = myLibrary[i];
-        let bookdiv = document.createElement("div");
-        bookdiv.innerHTML = `<p>${book.title}</p>`
-    }
-}
-display();
 // function bookItem() {
     
 
